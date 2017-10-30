51b7338bf341a3243ef7b2a2a6e8cdc8f0c5d4c3
Marcin Mrugacz <migacz85@gmail.com>
2017-10-30 13:12:28 +0000

    Add files via upload

---
+++ motivation_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+NUMBER=$(( ( RANDOM % 200 )  + 1 ))
+#echo "Sentencja numer: " $NUMBER
+
+p="p"
+q="'"
+#sed -n ${q}$NUMBER${p}${q} love_sentences.csv
+CMDTXT="sed -n ${q}$NUMBER${p}${q} /home/migacz/scripts/motivation.csv"
+eval "$CMDTXT"
+
+if [ $NUMBER -eq 50 ]
+then
+echo  "You should play in lotto - because chance of getting this message is 1 to 200 "
+fi
